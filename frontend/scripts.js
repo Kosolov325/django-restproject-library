d12fea7404b8f8199c362407beb9bbef0356abf8
Kosolov325 <xxlucas325xx@gmail.com>
2022-10-26 23:50:06 -0300

    deploy02

--- frontend/scripts.js
+++
@@ -1,7 +0,0 @@
-async function fetchUsers() {
-    const response = await fetch('https://library-project-backend.herokuapp.com/users/');
-    const data = await response.json()
-    console.log(data)
-  }
-
-  fetchUsers()
